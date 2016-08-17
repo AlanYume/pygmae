2ee4acbfe1a341c82ef46caaa61579b28b6f8385
AlanYume <875785679@qq.com>
2016-08-18 00:18:16 +0800

    move aliens

--- alien.py
+++ alien.py
@@ -22,9 +22,19 @@ class Alien(Sprite):
         # 存储外星人的准确位置
         self.x = float(self.rect.x)
 
-
     def blitme(self):
         """在指定位置绘制外星人"""
         self.screen.blit(self.image, self.rect)
 
+    def check_edges(self):
+        """如果外星人位于屏幕边缘,就返回 True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
 
+    def update(self):
+        """向左或向右移动外星人"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
