cbcd5d6a9c0715f3aa0b8d4ca7c7d811f6e5e893
AlanYume <875785679@qq.com>
2016-08-17 23:50:44 +0800

    drwa aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,15 +14,19 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个外星人
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    # 创建外星人群
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
