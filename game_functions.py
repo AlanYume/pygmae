91423bef109dbb8ef5f8f8545b6887a06246b800
AlanYume <875785679@qq.com>
2016-08-20 03:07:34 +0800

    hide mouse when playing game and show mouse when game over

--- game_functions.py
+++ game_functions.py
@@ -52,6 +52,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单击 Play 按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 隐藏光标
+        pygame.mouse.set_visible(False)
         stats.reset_stats()
         stats.game_active = True
         # 清空外星人列表和子弹列表
@@ -167,6 +169,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
